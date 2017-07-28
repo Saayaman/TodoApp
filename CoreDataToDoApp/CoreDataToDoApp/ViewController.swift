827d85027c8af6919b3fa1ab9ac72d1bfcaca7ac
Saayaman <ayakosayama1987@gmail.com>
2017-07-28 12:57:40 -0700

    todo app

--- CoreDataToDoApp/CoreDataToDoApp/ViewController.swift
+++ CoreDataToDoApp/CoreDataToDoApp/ViewController.swift
@@ -15,8 +15,8 @@ class ViewController: UIViewController {
     var todos:[Todo] = []
     
     @IBOutlet weak var deleteBtn: UIBarButtonItem!
-    let appDelegate = (UIApplication.shared.delegate as! AppDelegate)
-    var context:NSManagedObjectContext!
+    var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
     
     
     @IBOutlet weak var tableView: UITableView!
@@ -24,7 +24,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        context = appDelegate.persistentContainer.viewContext
         tableView.delegate = self
         tableView.dataSource = self
     }
@@ -35,11 +34,14 @@ class ViewController: UIViewController {
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Todo")
         do {
             todos = try context.fetch(fetchRequest as! NSFetchRequest<NSFetchRequestResult>) as! [Todo]
+            refreshTableView()
         }
         catch {
             print("Fetching Failed")
         }
-        
+    }
+    
+    func refreshTableView(){
         tableView.reloadData()
     }
     
@@ -56,50 +58,78 @@ class ViewController: UIViewController {
     
     @IBAction func addBtn(_ sender: UIBarButtonItem) {
         
+        let alert = UIAlertController(title: "AlertController Tutorial",
+                                      message: "Submit something",
+                                      preferredStyle: .alert)
+        
+        // Submit button
+        let submitAction = UIAlertAction(title: "Add", style: .default, handler: { (action) -> Void in
+            // Get 1st TextField's text
+            let textField = alert.textFields![0]
+            
+            let todo = Todo(context: self.context)
+            todo.task = textField.text
+            self.todos.append(todo)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        })
+        
+        // Cancel button
+        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        
+        // Add 1 textField and customize it
+        alert.addTextField { (textField: UITextField) in
+            textField.keyboardAppearance = .dark
+            textField.keyboardType = .default
+            textField.autocorrectionType = .default
+            textField.placeholder = "Type something here"
+            textField.clearButtonMode = .whileEditing
+        }
+        
+        // Add action buttons and present the Alert
+        alert.addAction(submitAction)
+        alert.addAction(cancel)
+        self.present(alert, animated: true, completion: nil)
+        
     }
-}
+  }
 
 
 extension ViewController: UITableViewDelegate{
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell") as! TableViewCell
-        
-        
-        if todos.count != 0 {
-            let todo = todos[indexPath.row]
-            if let task = todo.task{
-                cell.nameLabel.text = task
-            }
-        }
-        
-        return cell
-        
-    }
-    
-    
-}
-
-extension ViewController: UITableViewDataSource{
-    
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         
         if editingStyle == .delete{
             
-            if todos.count != 0 {
-
-            let todo = todos[indexPath.row]
-            context.delete(todo)
-            appDelegate.saveContext()
-                
-            }
+                let todo = todos[indexPath.row]
+                context.delete(todo)
+                todos.remove(at: indexPath.row)
+                (UIApplication.shared.delegate as! AppDelegate).saveContext()
         }
+        
+        refreshTableView()
     }
+
+    
+}
+
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let todo = todos[indexPath.row]
+        if let task = todo.task{
+            cell.textLabel?.text = task
+        }
+        
+        return cell
+        
+    }
+
 }
 
